2ae3afd59e9202f10a9163e5d737a9ba9db938c2
Voronskaya Darya <56038397+VoronDar@users.noreply.github.com>
2021-09-19 19:54:46 +0300

    add views xml

--- app/src/main/java/com/astery/xapplication/pojo/enums/AdviseType.java
+++ app/src/main/java/com/astery/xapplication/pojo/enums/AdviseType.java
@@ -3,7 +3,7 @@ package com.astery.xapplication.pojo.enums;
 /** consts for advises
  *
  * don't do - mistake but not fatal
- * rape - things that can harm you
+ * rape - your partner actions that can harm you
  * try-it - common ideas you might not use
  * always - that's pretty clear
  * be_carefull - call a doctor, be aware of your health
@@ -12,8 +12,7 @@ package com.astery.xapplication.pojo.enums;
 public class AdviseType {
     public static final int DO_NOT_DO_THAT_ANYMORE = 0;
     public static final int RAPE = 1;
-    public static final int TRY_IT = 2;
-    public static final int ALWAYS_DO_IT = 3;
-    public static final int BE_CAREFULL = 4;
-    public static final int NEW_IDEA = 5;
+    public static final int ALWAYS_DO_IT = 2;
+    public static final int BE_CAREFULL = 3;
+    public static final int NEW_IDEA = 4;
 }
